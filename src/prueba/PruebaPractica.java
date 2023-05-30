9ba47e352f8b9149af9fd5834f35a71fb7365804
Alumno1 <Alumno1@Desarrolo>
2023-05-31 01:32:22 +0200

    prueba

--- src/prueba/PruebaPractica.java
+++ src/prueba/PruebaPractica.java
@@ -4,6 +4,7 @@ public class PruebaPractica {
 
 
 	 //Cambio del alumno  2 (Nombre y Apellidos) 
+	
 // Constantes
 	private static final int EDAD_MAXIMA = 100;
 	private static final int LIMITE_NINNO = 12;
