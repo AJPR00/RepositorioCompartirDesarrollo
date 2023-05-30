ed6d2c9de9233dd8207d847b7d58e97a22e2afea
Lince <Lince@PaalVi>
2023-05-31 01:10:37 +0200

    1º cambio

--- src/prueba/PruebaPractica.java
+++ src/prueba/PruebaPractica.java
@@ -2,7 +2,8 @@ package prueba;
 import java.util.Scanner;
 public class PruebaPractica {
 
-	
+	 //Cambio del alumno  2 (Nombre y Apellidos) 
+
 // Constantes
 	private static final int EDAD_MAXIMA = 100;
 	private static final int LIMITE_NINNO = 12;
